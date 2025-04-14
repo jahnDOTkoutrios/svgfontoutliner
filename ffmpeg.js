08578812f9ecd2ba3be32d893c758dddcd28ee99
jahnDOTkoutrios <jahn.koutrios@gmail.com>
2025-04-14 10:35:24 +0200

    init

---
+++ ffmpeg.js
@@ -0,0 +1,3 @@
+// FFmpeg library code will be here
+// You'll need to download the FFmpeg library and place its contents here
+// You can get it from: https://unpkg.com/@ffmpeg/ffmpeg@0.12.7/dist/ffmpeg.min.js
